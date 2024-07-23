c5a2e35601df336859a2bcabcbfc8a9d4b91cae9
Paulo Sergio <036paulo@gmail.com>
2024-07-22 22:20:40 -0300

    adicionado persistencia dos dados no banco de dados, foi ajustado as anotacoes para contruir o banco de forma dinamica, salvar os dados e buscar todos os dados da pesquisa

--- java-screenmatch-jpa/src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ java-screenmatch-jpa/src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -3,6 +3,7 @@ package br.com.alura.screenmatch.principal;
 import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.model.DadosTemporada;
 import br.com.alura.screenmatch.model.Serie;
+import br.com.alura.screenmatch.repository.SerieRepository;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
 
@@ -20,6 +21,12 @@ public class Principal {
   private final String ENDERECO = "https://www.omdbapi.com/?t=";
   private final String API_KEY = "&apikey=1801fb5f";
   private List<DadosSerie> dadosSeries = new ArrayList<>();
+
+  private SerieRepository repository;
+
+  public Principal(SerieRepository repository) {
+    this.repository = repository;
+  }
 
   public void exibeMenu() {
     var opcao = -1;
@@ -55,7 +62,10 @@ public class Principal {
 
   private void buscarSerieWeb() {
     DadosSerie dados = getDadosSerie();
-    dadosSeries.add(dados);
+    // dadosSeries.add(dados);
+    Serie serie = new Serie(dados);
+
+    repository.save(serie);
     System.out.println(dados);
   }
 
@@ -68,11 +78,7 @@ public class Principal {
   }
 
   private void listarSeriesBuscadas(){
-    List<Serie> series = new ArrayList<>();
-
-    dadosSeries.stream()
-            .map(d -> new Serie(d))
-            .collect(Collectors.toList());
+    List<Serie> series = repository.findAll();
 
     series.stream()
             .sorted(Comparator.comparing(Serie::getGenero))
