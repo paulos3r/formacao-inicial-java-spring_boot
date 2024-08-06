e381d97dd7f419982ffed075e443ead9228ff649
Paulo Sergio <036paulo@gmail.com>
2024-08-05 22:22:14 -0300

    Iniciando o projeto final do curso inicio com spring web

---
+++ java-desafio-backend-main/src/main/java/paulos3r/java_desafio_front_main/JavaDesafioFrontMainApplication.java
@@ -0,0 +1,13 @@
+package paulos3r.java_desafio_front_main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDesafioFrontMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDesafioFrontMainApplication.class, args);
+	}
+
+}
---
+++ java-desafio-backend-main/src/test/java/paulos3r/java_desafio_front_main/JavaDesafioFrontMainApplicationTests.java
@@ -0,0 +1,13 @@
+package paulos3r.java_desafio_front_main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDesafioFrontMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
